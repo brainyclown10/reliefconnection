268e058b31769aca425e28eae769156a9b3a060c
Brandon Hu <brandon2009731@gmail.com>
2019-01-19 16:37:06 -0500

    Added correct file that allows you to declare the size of the generated list

--- randompointgenerator.py
+++ randompointgenerator.py
@@ -1,17 +1,18 @@
 import random
 random.seed()
 
+
 class LatLongList:
-    def __init__(self):
+    def __init__(self, num):
         self.l = []
-        for x in range(10):
+        for x in range(num):
             self.l.append((random.uniform(42.711747, 42.74863), random.uniform(-84.483571, -84.493784)))
 
     def getlist(self):
         return self.l
 
 
-pointslist = LatLongList()
+pointslist = LatLongList(10)
 li = pointslist.getlist()
 print(li)
 
